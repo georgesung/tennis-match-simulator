9505e6a666751c712e0d94a115a3a5028cca0ea5
Jou-ching (George) Sung <georgesung@gmail.com>
2024-10-06 16:36:16 +0100

    Make the icon show in prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  // If it's production, append the sub-path for GitHub Pages.
+  const faviconPath = isProd
+    ? "/tennis-match-simulator/favicon.ico"
+    : "/favicon.ico";
+
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={faviconPath} />
       </Head>
       <body className="antialiased">
         <Main />
