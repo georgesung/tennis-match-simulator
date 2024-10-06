ce54c454133481541413ead093300056b3f9df65
Jou-ching (George) Sung <georgesung@gmail.com>
2024-10-06 16:00:35 +0100

    Clean up comments, remove unused docs/ static assets

--- next.config.js
+++ next.config.js
@@ -1,37 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: 'export',
-
-//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//   // trailingSlash: true,
-
-//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//   // skipTrailingSlashRedirect: true,
-
-//   // Optional: Change the output directory `out` -> `dist`
-//   // distDir: 'dist',
-// }
-
-// // module.exports = nextConfig
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // for Github Pages
-  // distDir: 'docs',
+  // for Github Pages, this should match project name, e.g. https://www.georgesung.com/tennis-match-simulator/
   basePath: "/tennis-match-simulator",
 }
 
